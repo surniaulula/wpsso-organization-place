4b014414c086d0c8ae21f0f219a3404f957c7ddf
JS Morisset <jsm@surniaulula.com>
2021-12-21 08:20:16 -0800

    v1.1.1

--- lib/place/filters-messages-tooltip-meta.php
+++ lib/place/filters-messages-tooltip-meta.php
@@ -36,110 +36,110 @@ if ( ! class_exists( 'WpssoOpmPlaceFiltersMessagesTooltipMeta' ) ) {
 
 				case 'tooltip-meta-place_name':
 
-					$text = __( 'A name for this place (required).', 'wpsso-plm' ) . ' ';
+					$text = __( 'A name for this place (required).', 'wpsso-organization-place' ) . ' ';
 
-					$text .= __( 'The place name may appear in forms and in the Schema Place "name" property.', 'wpsso-plm' );
+					$text .= __( 'The place name may appear in forms and in the Schema Place "name" property.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_name_alt':
 
-					$text = __( 'An alternate name for this place.', 'wpsso-plm' ) . ' ';
+					$text = __( 'An alternate name for this place.', 'wpsso-organization-place' ) . ' ';
 
-					$text .= __( 'The place alternate name may appear in the Schema Place "alternateName" property.', 'wpsso-plm' );
+					$text .= __( 'The place alternate name may appear in the Schema Place "alternateName" property.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_desc':
 
-					$text = __( 'A description for this place.', 'wpsso-plm' ) . ' ';
+					$text = __( 'A description for this place.', 'wpsso-organization-place' ) . ' ';
 
-					$text .= __( 'The place description may appear in the Schema Place "description" property.', 'wpsso-plm' );
+					$text .= __( 'The place description may appear in the Schema Place "description" property.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_schema_type':	// Place Schema Type.
 
-					$text = __( 'You may optionally choose a different Schema type for this place (default is LocalBusiness).', 'wpsso-plm' );
+					$text = __( 'You may optionally choose a different Schema type for this place (default is LocalBusiness).', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_street_address':
 
-					$text = __( 'An optional street address for Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-plm' );
+					$text = __( 'An optional street address for Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_po_box_number':
 
-					$text = __( 'An optional post office box number for the Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-plm' );
+					$text = __( 'An optional post office box number for the Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_city':
 
-					$text = __( 'An optional city name for the Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-plm' );
+					$text = __( 'An optional city name for the Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_region':
 
-					$text = __( 'An optional state or province name for the Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-plm' );
+					$text = __( 'An optional state or province name for the Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_postal_code':
 
-					$text = __( 'An optional postal or zip code for the Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-plm' );
+					$text = __( 'An optional postal or zip code for the Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_country':
 
-					$text = __( 'An optional country for the Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-plm' );
+					$text = __( 'An optional country for the Pinterest Rich Pin / Schema Place meta tags and related markup.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_phone':
 
-					$text = __( 'An optional telephone number for this place.', 'wpsso-plm' );
+					$text = __( 'An optional telephone number for this place.', 'wpsso-organization-place' );
 
 					break;
 				case 'tooltip-meta-place_latitude':
 
-					$text = __( 'The numeric decimal degrees latitude for this place (required).', 'wpsso-plm' ) . ' ';
+					$text = __( 'The numeric decimal degrees latitude for this place (required).', 'wpsso-organization-place' ) . ' ';
 
-					$text .= __( 'You may use a service like <a href="http://www.gps-coordinates.net/">Google Maps GPS Coordinates</a> (as an example), to find the approximate GPS coordinates of a street address.', 'wpsso-plm' );
+					$text .= __( 'You may use a service like <a href="http://www.gps-coordinates.net/">Google Maps GPS Coordinates</a> (as an example), to find the approximate GPS coordinates of a street address.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_longitude':
 
-					$text = __( 'The numeric decimal degrees longitude for this place (required).', 'wpsso-plm' ) . ' ';
+					$text = __( 'The numeric decimal degrees longitude for this place (required).', 'wpsso-organization-place' ) . ' ';
 
-					$text .= __( 'You may use a service like <a href="http://www.gps-coordinates.net/">Google Maps GPS Coordinates</a> (as an example), to find the approximate GPS coordinates of a street address.', 'wpsso-plm' );
+					$text .= __( 'You may use a service like <a href="http://www.gps-coordinates.net/">Google Maps GPS Coordinates</a> (as an example), to find the approximate GPS coordinates of a street address.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_altitude':
 
-					$text = __( 'An optional numeric altitude (in meters above sea level) for this place.', 'wpsso-plm' );
+					$text = __( 'An optional numeric altitude (in meters above sea level) for this place.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_img_id':	// Place Image ID.
 
-					$text = __( 'An image of this place (ie. an image of the business storefront or location).', 'wpsso-plm' ) . ' ';
+					$text = __( 'An image of this place (ie. an image of the business storefront or location).', 'wpsso-organization-place' ) . ' ';
 
-					$text .= '<em>' . __( 'This option is disabled if a place image URL is entered.', 'wpsso-plm' ) . '</em>';
+					$text .= '<em>' . __( 'This option is disabled if a place image URL is entered.', 'wpsso-organization-place' ) . '</em>';
 
 					break;
 
 				case 'tooltip-meta-place_img_url':	// or Place Image URL.
 
-					$text = __( 'You can enter a place image URL (including the http:// prefix) instead of selecting an image ID.', 'wpsso-plm' ) . ' ';
+					$text = __( 'You can enter a place image URL (including the http:// prefix) instead of selecting an image ID.', 'wpsso-organization-place' ) . ' ';
 
-					$text .= __( 'The image URL option allows you to specify an image URL outside of the WordPress Media Library and/or a smaller logo style image.', 'wpsso-plm' ) . ' ';
+					$text .= __( 'The image URL option allows you to specify an image URL outside of the WordPress Media Library and/or a smaller logo style image.', 'wpsso-organization-place' ) . ' ';
 
 					$text .= '<em>' . __( 'This option is disabled if a place image ID is selected.', 'wpsso' ) . '</em>';
 
@@ -147,71 +147,71 @@ if ( ! class_exists( 'WpssoOpmPlaceFiltersMessagesTooltipMeta' ) ) {
 
 				case 'tooltip-meta-place_timezone':	// Place Timezone.
 
-					$text = __( 'A timezone for the place open and close hours.', 'wpsso-plm' ) . ' ';
+					$text = __( 'A timezone for the place open and close hours.', 'wpsso-organization-place' ) . ' ';
 
-					$text .= __( 'The default timezone is provided by WordPress.', 'wpsso-plm' );
+					$text .= __( 'The default timezone is provided by WordPress.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_days':		// Open Days / Hours.
 
-					$text = __( 'Select the days and hours this place is open.', 'wpsso-plm' );
+					$text = __( 'Select the days and hours this place is open.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_season_dates':	// Seasonal Dates.
 
-					$text = __( 'If this place is open seasonally, select the open and close dates of the season.', 'wpsso-plm' );
+					$text = __( 'If this place is open seasonally, select the open and close dates of the season.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_service_radius':
 
-					$text = __( 'The geographic area where a service is provided, in meters around the location.', 'wpsso-plm' );
+					$text = __( 'The geographic area where a service is provided, in meters around the location.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_currencies_accepted':
 
-					$text = sprintf( __( 'A comma-delimited list of <a href="%1$s">ISO 4217 currency codes</a> accepted by the local business (example: %2$s).', 'wpsso-plm' ), 'https://en.wikipedia.org/wiki/ISO_4217', 'USD, CAD' );
+					$text = sprintf( __( 'A comma-delimited list of <a href="%1$s">ISO 4217 currency codes</a> accepted by the local business (example: %2$s).', 'wpsso-organization-place' ), 'https://en.wikipedia.org/wiki/ISO_4217', 'USD, CAD' );
 
 					break;
 
 				case 'tooltip-meta-place_payment_accepted':
 
-					$text = __( 'A comma-delimited list of payment options accepted by the local business (example: Cash, Credit Card).', 'wpsso-plm' );
+					$text = __( 'A comma-delimited list of payment options accepted by the local business (example: Cash, Credit Card).', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_price_range':
 
-					$text = __( 'The relative price of goods or services provided by the local business (example: $, $$, $$$, or $$$$).', 'wpsso-plm' );
+					$text = __( 'The relative price of goods or services provided by the local business (example: $, $$, $$$, or $$$$).', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_accept_res':
 
-					$text = __( 'This food establishment accepts reservations.', 'wpsso-plm' );
+					$text = __( 'This food establishment accepts reservations.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_menu_url':
 
-					$text = __( 'The menu URL for this food establishment.', 'wpsso-plm' );
+					$text = __( 'The menu URL for this food establishment.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_cuisine':
 
-					$text = __( 'The cuisine served by this food establishment.', 'wpsso-plm' );
+					$text = __( 'The cuisine served by this food establishment.', 'wpsso-organization-place' );
 
 					break;
 
 				case 'tooltip-meta-place_order_urls':
 
-					$text = __( 'A comma-delimited list of website and mobile app URLs to order products.', 'wpsso-plm' ) . ' ';
+					$text = __( 'A comma-delimited list of website and mobile app URLs to order products.', 'wpsso-organization-place' ) . ' ';
 
-					$text .= __( 'These order action URL(s) will be used in the Schema potentialAction property.', 'wpsso-plm' );
+					$text .= __( 'These order action URL(s) will be used in the Schema potentialAction property.', 'wpsso-organization-place' );
 
 					break;
 			}
