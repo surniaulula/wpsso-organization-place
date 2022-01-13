dfc81359f9b096cdc4147cdf33d1d2148b94462e
JS Morisset <jsm@surniaulula.com>
2022-01-13 08:43:11 -0800

    v1.2.1

--- wpsso-organization-place.php
+++ wpsso-organization-place.php
@@ -14,7 +14,7 @@
  * Requires PHP: 7.2
  * Requires At Least: 5.2
  * Tested Up To: 5.8.3
- * Version: 1.2.1-rc.1
+ * Version: 1.2.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
