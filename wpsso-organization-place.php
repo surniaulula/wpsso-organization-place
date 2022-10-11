b1351e976492baf2fbdc77ebe40a57ad7ead47dd
JS Morisset <jsm@surniaulula.com>
2022-10-11 06:49:09 -0700

    v1.6.0

--- wpsso-organization-place.php
+++ wpsso-organization-place.php
@@ -13,7 +13,7 @@
  * Description: Manage Organizations (publisher, organizer, etc.) and Places for Facebook, Pinterest, and Google local business markup.
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 6.0.2
+ * Tested Up To: 6.1.0
  * Version: 1.6.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
