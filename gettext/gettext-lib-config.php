5c0ac2a60854aa59762ea3fc80c4df85a2adfb6f
JS Morisset <jsm@surniaulula.com>
2022-12-28 12:04:56 -0800

    v1.7.0

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -1,8 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable strings from lib/config.php.
- */
+// 
+// Extract of translatable strings from lib/config.php.
+// 
 _x( 'WPSSO Organization and Place Manager', 'plugin name', 'wpsso-organization-place' );
 _x( 'Manage Organizations (publisher, organizer, etc.) and Places for Facebook, Pinterest, and Google local business markup.', 'plugin description', 'wpsso-organization-place' );
 _x( 'WPSSO Core', 'plugin name', 'wpsso-organization-place' );
