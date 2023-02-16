7d193d3253faacf4ffb0c3bc85122b7a2785cc45
JS Morisset <jsm@surniaulula.com>
2023-02-16 14:19:18 -0800

    v1.11.1-dev.3

--- lib/config.php
+++ lib/config.php
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoOpmConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '15.3.1-dev.2',
+							'min_version'   => '15.3.1-dev.3',
 						),
 					),
 
