a54e0b0538d9d3c45784b279dea1e697421885e1
JS Morisset <jsm@surniaulula.com>
2023-10-18 07:36:56 -0700

    v1.15.0

--- wpsso-organization-place.php
+++ wpsso-organization-place.php
@@ -14,7 +14,7 @@
  * Requires Plugins: wpsso
  * Requires PHP: 7.2.34
  * Requires At Least: 5.5
- * Tested Up To: 6.3.2
+ * Tested Up To: 6.4.0
  * Version: 1.15.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
