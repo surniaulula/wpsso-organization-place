fb4cf8a1d8c1b227e96200635311683b2abd4d52
jsmoriss <jsm@surniaulula.com>
2024-07-15 05:24:41 +0000

    v2.4.0

--- lib/org/filters-options.php
+++ lib/org/filters-options.php
@@ -140,6 +140,9 @@ if ( ! class_exists( 'WpssoOpmOrgFiltersOptions' ) ) {
 			return $md_opts;
 		}
 
+		/*
+		 * Return the sanitation type for a given option key.
+		 */
 		public function filter_option_type( $type, $base_key ) {
 
 			if ( ! empty( $type ) ) {	// Return early if we already have a type.
