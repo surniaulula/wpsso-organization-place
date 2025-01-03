1221dac9fbb8b54182a2b0ef3c2e22bf87518a1c
jsmoriss <jsm@surniaulula.com>
2025-01-03 01:55:44 +0000

    v3.2.2

--- wpsso-organization-place.php
+++ wpsso-organization-place.php
@@ -15,7 +15,7 @@
  * Requires PHP: 7.4.33
  * Requires At Least: 5.9
  * Tested Up To: 6.7.1
- * Version: 3.2.2-rc.1
+ * Version: 3.2.2
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
