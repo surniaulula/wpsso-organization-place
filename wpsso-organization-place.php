7455a727b3c106f0ca08f555542ecfd7fca425e1
jsmoriss <jsm@surniaulula.com>
2025-07-15 20:43:24 +0000

    v5.1.0

--- wpsso-organization-place.php
+++ wpsso-organization-place.php
@@ -14,7 +14,7 @@
  * Requires Plugins: wpsso
  * Requires PHP: 7.4.33
  * Requires At Least: 5.9
- * Tested Up To: 6.8.1
+ * Tested Up To: 6.8.2
  * Version: 5.1.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
